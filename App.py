ef46d99988eed3cf5e4e7d129011794f68dfcddc
Deeksha L D <1nc25mc005@ncetmail.com>
2026-04-15 10:35:01 +0530

    Update App.py

--- App.py
+++ App.py
@@ -1,5 +1,5 @@
 import streamlit as st
 from groq import Groq
-st.set_page_config("DeekshaAI content Generator",layout="wide")
-st.title("deekshaAI-content Generator")
-st.image("")
+st.set_page_config("AI content Generator",layout="wide")
+st.title("AI-content Generator")
+st.image("OIP.jpg")
